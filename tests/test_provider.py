c90174800033490e47ddbd110b7a4b8f063c0d93
Matt Rixman <MatrixManAtYrService@users.noreply.github.com>
2022-12-03 13:09:42 -0700

    fix test

--- tests/test_provider.py
+++ tests/test_provider.py
@@ -2,4 +2,5 @@ import dagfs
 import re
 
 def test_provider_info():
-    assert re.match(r"[0-9]\.[0-9]\.[0-9].*", dagfs.get_provider_info()["version"]
+    print(dagfs.get_provider_info()["version"])
+    assert re.match(r"[0-9]+\.[0-9]+\.[0-9]+.*", dagfs.get_provider_info()["version"])
